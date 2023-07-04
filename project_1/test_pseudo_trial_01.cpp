9b69d3317b71ae83d5d76e134bb3056aaa6187b3
sajurr <depsitesagar@gmail.com>
2023-07-04 12:09:43 +0530

    Fourth commit

--- project_1/test_pseudo_trial_01.cpp
+++ project_1/test_pseudo_trial_01.cpp
@@ -18,24 +18,28 @@ int main()
     pythia.readString("Beams:idA = 2212");
     pythia.readString("Beams:idB = 2212");
     pythia.readString("SoftQCD:all = on");
-    pythia.readString("Beams:eCM = 900");
+    pythia.readString("Beams:eCM = 7000");
     pythia.init();
 
+    // Declaration of variables and vectors used in the event loop
     double pTMin = 0.3;
     double pTMax = 1.5;
-    int eventNumber = 10000;
+    int eventNumber = 50000;
     vector<double> window_width = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8};
     vector<double> fwdMult(window_width.size(), 0.0);
     vector<double> bkwdMult(window_width.size(), 0.0);
 
     // Create histograms and profiles
+    
+    /*Scatter plots of eta and pT to see if the event loop runs fine*/
     TH1F hist("hist", "pT", 100, pTMin, pTMax);
     TH1F hist2("hist2", "eta", 100, -3, 3);
 
+    /*Plot of b_corr versus n_gap and its TProfile*/
     TH2F b_corr_vs_width("b_corr_vs_width", "b_corr vs Window Width", window_width.size() - 2, &window_width[1], 100, 0, 1.2);
     TProfile profile_b_corr("profile_b_corr", "b_corr vs Window Width", window_width.size() - 2, &window_width[1]);
 
-    // Event loop
+    // Event loop begins
     for (int iEvents = 0; iEvents < eventNumber; ++iEvents)
     {
         if (!pythia.next())
@@ -48,7 +52,7 @@ int main()
         // Particle loop
         for (int i = 0; i < pythia.event.size(); ++i)
         {
-            // Conditional statement
+            // Declaring cut on transverse momentum
             if (!pythia.event[i].isCharged() || !pythia.event[i].isFinal())
                 continue;
             if (pythia.event[i].pT() < pTMin || pythia.event[i].pT() > pTMax)
@@ -58,7 +62,7 @@ int main()
             hist.Fill(pythia.event[i].pT());
             hist2.Fill(pythia.event[i].eta());
 
-            // Choosing forward multiplicity within different pseudorapidity windows
+            // Choosing and storing forward multiplicity within different pseudorapidity windows
             for (int j = 0; j < window_width.size() - 1; ++j) {
                 if ((pythia.event[i].eta()) >= window_width[j] && (pythia.event[i].eta()) < window_width[j + 1]) {
                     fwdMult[j]++;
@@ -66,49 +70,16 @@ int main()
 
                 }
             }
-            // Choosing backward multiplicity within different pseudorapidity windows
+            // Choosing and storing backward multiplicity within different pseudorapidity windows
             for (int j = 0; j < window_width.size() - 1; ++j) {
                 if ((pythia.event[i].eta()) <= -window_width[j] && (pythia.event[i].eta()) > -window_width[j + 1]) {
                     bkwdMult[j]++;
                     break;
                 }
             }
-        }    
-            // // Print the entries of bkwdMult vector
-            // for (int j = 0; j < bkwdMult.size(); ++j) {
-            //     cout << "bkwdMult[" << j << "] = " << bkwdMult[j] << endl;
-            // }
+        }    // Particle loop ends
 
-            //     // Choosing forward multiplicity within different pseudorapidity windows
-            //     for (int j = 0; j < window_width.size() - 1; ++j)
-            //     {
-            //         if (abs(pythia.event[i].eta()) < window_width[j + 1])
-            //         {
-            //             fwdMult[j]++;
-            //             break;
-            //         }
-            //     }
-
-            //     // Choosing backward multiplicity within different pseudorapidity windows
-            //     for (int j = 0; j < window_width.size() - 1; ++j)
-            //     {
-            //         if (abs(pythia.event[i].eta()) > 2.8 - window_width[j + 1])
-            //         {
-            //             bkwdMult[j]++;
-            //             break;
-            //         }
-            //     }
-            // }
-            
-
-            //Print the entries of fwdMult vector
-            // for (int j = 0; j < fwdMult.size(); ++j) {
-            //     cout << "Event number "<<iEvents<<" fwd[" << j << "] = " << fwdMult[j] << endl;
-            // }
-            // for (int j = 0; j < bkwdMult.size(); ++j) {
-            //     cout << "Event number "<<iEvents<<" bkwd[" << j << "] = " << bkwdMult[j] << endl;
-            // }
-
+            // Initialization of variables used in b_corr calculation
             double sum_nF_k = 0.0;
             double sum_nB_k = 0.0;
             double sum_nBnF_k = 0.0;
@@ -128,15 +99,18 @@ int main()
             sum_nB_k += bkwdMult[k];
             sum_nBnF_k += fwdMult[k] * bkwdMult[k];
 
-            double b_corr = (sum_nBnF_k / 10000 - (sum_nF_k * sum_nB_k) / 10000) / (nFSquared_sum_k / 10000 - (sum_nF_k * sum_nF_k) / 10000);
-
+            double b_corr = (sum_nBnF_k / eventNumber - (sum_nF_k * sum_nB_k) / eventNumber) / (nFSquared_sum_k / eventNumber - (sum_nF_k * sum_nF_k) / eventNumber);
+            /*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+            // Since we are taking TProfile of b_corr, I suppose there is no need to divide by eventNumber in the calculation of b_corr. NEED TO VERIFY !!
+            /*------------------------------------------------------------------------------------------------------------------------------------------------------*/
             if (std::isnan(b_corr) || std::isinf(b_corr))
                 continue;
 
             b_corr_vs_width.Fill(window_width[k], b_corr);
             profile_b_corr.Fill(window_width[k], b_corr);
             }
-    }
+    }   // Event loop ends
+
     // Create a ROOT file and save the histograms
     TFile outFile("output.root", "RECREATE");
     hist.Write();
